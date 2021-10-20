f3cc232dd8ee8d12408b16aed53595bb5526f6e7
cherryVsV <62546662+cherryVsV@users.noreply.github.com>
2021-10-20 12:04:28 +0300

    Update FileController.php

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\ProjectExceptions\ValidationDataError;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 
 class FileController extends Controller
@@ -13,13 +14,12 @@ class FileController extends Controller
         if (!auth()->check()) {
             throw new ValidationDataError('ERROR_AUTHORIZATION_CHECK_FAILED', 401, 'Unauthorized');
         }
-        $this->validate($request, [
+       /* $this->validate($request, [
             'image' => 'required|mimes:jpg,png,jpeg,bmp|max:2048'
-        ]);
+        ]);*/
         if($request->hasFile('image')) {
-            $image = time().rand() . '.' . $request->image->getClientOriginalExtension();
-            $request->image->move(public_path('images/photos'), $image);
-            return response()->json(['imageURL'=>URL::to('/') . '/public/images/photos/' .$image]);
+            $path = Storage::disk('public')->put('images/photos', $request->image);
+            return response()->json(['imageURL'=>URL::to('/') . '/storage/'.$path]);
         }else{
            throw new ValidationDataError('UPLOAD_IMAGE_ERROR', 422, 'Image can not be uploaded');
         }
