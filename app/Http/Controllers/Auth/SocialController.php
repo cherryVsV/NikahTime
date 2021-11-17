2b5e283d342485381348548919b2401e78be3559
cherryVsV <62546662+cherryVsV@users.noreply.github.com>
2021-11-17 16:57:39 +0300

    Update SocialController.php

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -25,17 +25,16 @@ class SocialController extends Controller
     {
         try {
             $code = $request->code;
-            $endpoint = "https://appleid.apple.com/auth/token";
             $client = new Client();
-            $response = $client->request('POST', $endpoint, ['query' => [
+
+            $response = $client->request('POST', 'https://appleid.apple.com/auth/token', ['form_params' => [
                 'client_id' => "ru.nikahtime.web",
                 'client_secret' => "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjVEUDdITTc3VDcifQ.eyJpc3MiOiJaM1k1TDhIVkRCIiwiaWF0IjoxNjM3MTMxODIwLCJleHAiOjE2NTI2ODM4MjAsImF1ZCI6Imh0dHBzOi8vYXBwbGVpZC5hcHBsZS5jb20iLCJzdWIiOiJydS5uaWthaHRpbWUud2ViIn0.fvgIqQoKDt7z0sFo87-L5e77onGyzybS0DyJSnFRfnL8hJXPUvDoRzykTfNvSB4JxJd8XTmbjshT-1lT2pOvGw",
                 'code' => $code,
                 'grant_type' => 'authorization_code'
             ]]);
-            $statusCode = $response->getStatusCode();
-            logger($statusCode);
-            $content = json_decode($response->getBody());
+
+            $content = json_decode($response->getBody(), true);
             $idToken = $content['id_token'];
             $register = new LoginAndRegisterViaGoogleService();
             $userData = $register->authViaGoogle($idToken, 'apple');
