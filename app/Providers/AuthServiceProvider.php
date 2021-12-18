a76b2922817a2656e77d86c3fe426d8bf62e8692
cherryVsV <62546662+cherryVsV@users.noreply.github.com>
2021-12-18 10:58:17 +0300

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
-        Passport::tokensExpireIn(Carbon::now()->addDays(1));
+        Passport::tokensExpireIn(Carbon::now()->addDays(10));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
     }
 }
